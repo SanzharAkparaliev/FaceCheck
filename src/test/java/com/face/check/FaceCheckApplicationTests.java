f1e1da376b06e6962e33a786768ef6f598dff57d
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2024-01-11 11:14:45 +0600

    first commit

---
+++ src/test/java/com/face/check/FaceCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.face.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
